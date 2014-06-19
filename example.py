38c2be1ef51545e486db7882ec7dce010730edc0
Daniel Bailey <danieljabailey@gmail.com>
2014-06-19 14:23:56 +0000

    Fixed decimal point

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ import time
 
 myDisplay = FourCharDisplay(0x70)
 
-myDisplay.write(" Go");
+myDisplay.write("....");
 
 time.sleep(3)
 
@@ -15,20 +15,11 @@ while 1:
 			myDisplay.FOUR_CHAR_DISP_ON | 
 			myDisplay.FOUR_CHAR_DISP_BLINK_OFF)
 
-	myDisplay.write("TEST");
-
-	time.sleep(2)
-
-	myDisplay.write("1Hz");
-	myDisplay.set_register(myDisplay.FOUR_CHAR_REG_DISP_SET | 
-			myDisplay.FOUR_CHAR_DISP_ON | 
-			myDisplay.FOUR_CHAR_DISP_BLINK_1HZ)
-
+	myDisplay.write(chr(0)+chr(1)+chr(2)+chr(3));
 	time.sleep(5)
-
-	myDisplay.write("2Hz");
-	myDisplay.set_register(myDisplay.FOUR_CHAR_REG_DISP_SET | 
-			myDisplay.FOUR_CHAR_DISP_ON | 
-			myDisplay.FOUR_CHAR_DISP_BLINK_2HZ)
-
+	myDisplay.write(chr(4)+chr(5)+chr(6)+chr(7));
+	time.sleep(5)
+	myDisplay.write(chr(8)+chr(9)+chr(10)+chr(11));
+	time.sleep(5)
+	myDisplay.write(chr(12)+chr(13)+chr(14)+chr(15));
 	time.sleep(5)
